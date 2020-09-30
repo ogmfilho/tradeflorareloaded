bd7eb134b7e08e55a2f05205fa6294ab543f36e3
luis cavalcanti <felipevillarc@hotmail.com>
2020-09-30 16:25:22 -0300

    model do user e migration com novas colunas

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reviews
+  has_many :trades
+  has_many :areas
+
+  validates :name, :document_number, :phone_number, :address,  presence: true
 end
