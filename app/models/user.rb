e1926ad78217ed6aa45f66365a9a9b8b96372950
Otoniel Miranda <ogmfilho@gmail.com>
2020-09-30 16:53:47 -0300

    Validações de user. Modelo Review, com validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews
+  has_many :trades, dependent: :destroy
+  has_many :areas, dependent: :destroy
+
+  validates :name, :document_number, :phone_number, :address, presence: true
 end
