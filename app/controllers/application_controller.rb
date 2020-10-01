c096ffa9ebd579d87387119483de6b5c2c75c016
luis cavalcanti <felipevillarc@hotmail.com>
2020-10-01 16:33:59 -0300

    card-br e botoes-br nas outras páginas do devise e atualizacao do applicationController para aceitar os atributos especificos de User q n vem no devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :document_number, :address, :phone_number, :city_id])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :document_number, :address, :phone_number, :city_id])
+  end
 end
