d2dbcf27936ba27643411b9a7fe440fdc039c41b
Otoniel Miranda <ogmfilho@gmail.com>
2020-10-01 14:45:45 -0300

    controller trades + views

---
+++ app/controllers/trades_controller.rb
@@ -0,0 +1,35 @@
+class TradesController < ApplicationController
+
+  def new
+    @trade = Trade.new
+    @area = Area.find(params[:area_id])
+  end
+
+  def create
+    @trade = Trade.new(trade_params)
+    if @trade.save
+      area = Area.find(@trade.area_id)
+      # area.status = true      mudar o nome de status
+      # if area.save
+      #  redirect_to user_path(area.user_id), notice: 'Nova trade criada.'
+        # end
+      redirect_to my_trades_path, notice: 'Nova trade criada.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trade = Trade.find(params[:id])
+
+    @trade.destroy
+
+    redirect_to my_proposals_path, notice: 'Proposta recusada.'
+  end
+
+  private
+
+  def trade_params
+    params.require(:trade).permit(:user_id, :area_id, :status, :viewed)
+  end
+end
