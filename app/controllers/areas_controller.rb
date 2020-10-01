811b03ab9264bac3911cd0ea719584e292f7631f
luis cavalcanti <felipevillarc@hotmail.com>
2020-10-01 12:14:26 -0300

    rota e controller de area e views index e show

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,75 @@
+class AreasController < ApplicationController
+  #tudo copiado do tradeflorabeta
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  
+  def index
+    if params[:query].present?
+      @areas = Area.search_by_area_user_or_basin(params[:query])
+    else
+      @areas = Area.all
+    end
+    @areas.where(recovery:nil) 
+  end
+
+  def show
+    @basins = Basin.all
+    @area = Area.find(params[:id])
+    @markers = [{
+        lat: @area.latitude,
+        lng: @area.longitude
+      }]
+  end
+
+  def new
+    @area = Area.new
+    @basins = Basin.all
+  end
+
+  def edit
+    @area = Area.find(params[:id])
+    @basins = Basin.all
+    #raise
+  end
+
+  def create
+    @area = Area.new(area_params)
+    @area.user = current_user
+
+    if @area.save!
+      redirect_to area_path(@area), notice: 'Nova área criada.'
+    else
+      @basins = Basin.all
+      render :new
+    end
+  end
+
+  def update
+      @area = Area.find(params[:id])
+      if @area.update!(area_params)
+        redirect_to area_path(@area), notice: 'Área modificada.'
+      else
+        @basins = Basin.all
+        render :edit
+    end
+  end
+
+  def destroy
+    @area = Area.find(params[:id])
+    @area.destroy
+
+    redirect_to @area, notice: 'Área removida.'
+  end
+
+  def aprove
+    @area = Area.find(params[:area_id])
+    @area.update(recovery: true)
+
+    redirect_to @area
+  end
+
+  private
+
+  def area_params
+    params.require(:area).permit(:lat, :long, :description, :basin_id)
+  end
+end
