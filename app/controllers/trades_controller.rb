4991ab03b58e5c5da20fe258e2b6881a4809211e
Otoniel Miranda <ogmfilho@gmail.com>
2020-10-01 17:26:51 -0300

    Trade new funcionando

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -3,17 +3,20 @@ class TradesController < ApplicationController
   def new
     @trade = Trade.new
     @area = Area.find(params[:area_id])
+    # raise
   end
 
   def create
     @trade = Trade.new(trade_params)
+    @area = Area.find(params[:area_id])
     if @trade.save
       area = Area.find(@trade.area_id)
       # area.status = true      mudar o nome de status
       # if area.save
       #  redirect_to user_path(area.user_id), notice: 'Nova trade criada.'
         # end
-      redirect_to my_trades_path, notice: 'Nova trade criada.'
+      # redirect_to my_trades_path, notice: 'Nova trade criada.'
+      redirect_to root_path, notice: 'Nova trade criada.'
     else
       render :new
     end
