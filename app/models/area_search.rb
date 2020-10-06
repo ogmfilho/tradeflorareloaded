678a71e19540e76f301ae34c12af3f1b4a39f069
luis cavalcanti <felipevillarc@hotmail.com>
2020-10-06 10:29:08 -0300

    correcao na pesquisa erro em area-max e area-min

--- app/models/area_search.rb
+++ app/models/area_search.rb
@@ -3,49 +3,50 @@ class AreaSearch < ApplicationRecord
   
 
   def pesquisa
-    @areas_after_states = []
-    @areas_after_cities = []
-    @areas_after_basins = []
-    @areas = Area.all
-    @areas = @areas.search_by_area_user_or_basin(keywords) if keywords.present?
+    areas_after_states = []
+    areas_after_cities = []
+    areas_after_basins = []
+    areas = Area.all
+    areas = areas.search_by_area_user_or_basin(keywords) if keywords.present?
+    
+    if !areas.empty? && min_extension.present?
+      areas = areas.where("extension >= ?", min_extension ) 
+    end
+
+    if !areas.empty? && max_extension.present?
+      areas = areas.where("extension <= ?", max_extension )
+    end
     
     states_id.reject!(&:empty?)
-    if states_id.present? && !@areas.empty?
-      states_id.each { |state_id| @areas_after_states << @areas.search_by_state(state_id) }
-      @areas = @areas_after_states.flatten
+    if states_id.present? && !areas.empty?
+      states_id.each { |state_id| areas_after_states << areas.search_by_state(state_id) }
+      areas = areas_after_states.flatten
     end
     
     cities_id.reject!(&:empty?)
-    if cities_id.present? && !@areas.empty?
-      if !@areas_after_states.flatten.empty?
+    if cities_id.present? && !areas.empty?
+      if !areas_after_states.flatten.empty?
         cities_id.each do |city_id|
-          unless @areas_after_states.flatten.include?(city_id)
+          unless areas_after_states.flatten.include?(city_id)
             
-            @areas = []
-            return @areas
+            areas = []
+            return areas
           end
         end
       end
-      cities_id.each { |city_id| @areas_after_cities << @areas.search_by_city(city_id) }
+      cities_id.each { |city_id| areas_after_cities << areas.search_by_city(city_id) }
       
-      @areas =  @areas_after_cities.flatten
+      areas =  areas_after_cities.flatten
     end
     
     basins_id.reject!(&:empty?)
-    if basins_id.present? && !@areas.empty?
-      basins_id.each {|basin_id| @areas_after_basins << @areas.search_by_basin(basin_id) }
-      @areas = @areas_after_basins.flatten
+    if basins_id.present? && !areas.empty?
+      basins_id.each {|basin_id| areas_after_basins << areas.search_by_basin(basin_id) }
+      areas = areas_after_basins.flatten
     end
     
-    if !@areas.empty? && min_extension.present?
-      @areas = @areas.where("extension >= ?", min_extension ) 
-    end
-
-    if !@areas.empty? && max_extension.present?
-      @areas = @areas.where("extension <= ?", max_extension )
-    end
     
-    return @areas
+    return areas
   end
 
   def plural(number, word)
