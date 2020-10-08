0be1487bb4b6768b7e6979f2a7622668da85c25a
watila machado <watilapm@gmail.com>
2020-10-08 17:08:52 -0300

    Configurado para mandar pelo gmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'contato@tradeflora.com'
+  default from: 'tradeflora.br@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.delivery_method     = :smtp
   config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
   # Settings specified here will take precedence over those in config/application.rb.
 
