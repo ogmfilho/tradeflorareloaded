625b61381683ffc61f8a58ce5d6c22d6b752eb21
luis cavalcanti <felipevillarc@hotmail.com>
2020-10-08 10:08:39 -0300

    correcao pesquisa

--- app/models/area_search.rb
+++ app/models/area_search.rb
@@ -17,38 +17,40 @@ class AreaSearch < ApplicationRecord
       areas = areas.where("extension <= ?", max_extension )
     end
     
-    states_id.reject!(&:empty?)
-    if states_id.present? && !areas.empty?
-      states_id.each { |state_id| areas_after_states << areas.search_by_state(state_id) }
-      areas = areas_after_states.flatten
-    end
     
     cities_id.reject!(&:empty?)
     if cities_id.present? && !areas.empty?
-      if !areas_after_states.flatten.empty?
-        cities_id.each do |city_id|
-          unless areas_after_states.flatten.include?(city_id)
-            
-            areas = []
-            return areas
-          end
-        end
-      end
-      cities_id.each { |city_id| areas_after_cities << areas.search_by_city(city_id) }
+      areas = areas.where(city_id: cities_id)
+      #if !areas_after_states.flatten.empty?
+      # cities_id.each do |city_id|
+      #  unless areas_after_states.flatten.include?(city_id)
       
-      areas =  areas_after_cities.flatten
+      #   areas = []
+      #  return areas
+      # end
+      # end
+      #end
+      # cities_id.each { |city_id| areas_after_cities << areas.search_by_city(city_id) }
+      
+      # areas =  areas_after_cities.flatten
     end
     
     basins_id.reject!(&:empty?)
     if basins_id.present? && !areas.empty?
-      basins_id.each {|basin_id| areas_after_basins << areas.search_by_basin(basin_id) }
-      areas = areas_after_basins.flatten
+      areas = areas.where(basin_id: basins_id)
+      
     end
     
+    states_id.reject!(&:empty?)
+    if states_id.present? && !areas.empty?
+      states_id.each { |state_id| areas_after_states << areas.search_by_state(state_id) }
+      areas = areas_after_states.flatten
+      #areas = areas.where("state_id = ?", states_id)
+    end
     
     return areas
   end
-
+  
   def plural(number, word)
     if number> 1
       return "Algum dos #{number} #{word}s:" unless word.include?(" ")
